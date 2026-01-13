684573f3859b366e2913ef6494d5f9440d65b3c9
arka <arkagarai292@gmail.com>
2026-01-14 01:07:24 +0530

    do reverse LL

--- _05 LinkedList - EasyMedium/_08ReverseLinkedList.js
+++ _05 LinkedList - EasyMedium/_08ReverseLinkedList.js
@@ -1,5 +1,3 @@
-//Given the head of a singly linked list, reverse the list, and return the reversed list.
-
 var reverseList = function (head) {
   let prev = null;
   let curr = head;
@@ -11,3 +9,38 @@ var reverseList = function (head) {
   }
   return prev;
 };
+
+function Node(val) {
+  this.val = val;
+  this.next = null;
+}
+
+let node1 = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
+let node4 = new Node(4);
+let node5 = new Node(5);
+
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
+let head = node1;
+
+function printList(head) {
+  let curr = head;
+  let res = [];
+  while (curr !== null) {
+    res.push(curr.val);
+    curr = curr.next;
+  }
+  console.log(res.join(" -> "));
+}
+
+console.log("Before reverse:");
+printList(head);
+
+head = reverseList(head);
+
+console.log("After reverse:");
+printList(head);
